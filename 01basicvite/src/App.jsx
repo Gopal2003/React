c20292ec1b58506669865d064a8b0c36eb0631b2
Gopal <gopalmittal2003@gmail.com>
2024-09-28 17:24:44 +0530

    Basic Code Structure Understanding

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <h1>
+     
+      Hello this is a sample text
+     
+    </h1>
+  )
+}
+
+export default App
