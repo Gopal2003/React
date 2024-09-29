94eb3f6c8109eb6412a69d0fe4d5364cfe0d1fbc
Gopal <gopalmittal2003@gmail.com>
2024-09-29 20:01:41 +0530

    CustomReact and Deep dive into the inner portion of working of react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -3,14 +3,18 @@ import Coffee from './coffee.jsx'
 
 
 function App() {
-
+  const username = "Gopal Agarwal"
 //! ONLY one tag is returned here. hence, we use <> </> which is called as fragment tag.
   return (
+    //! {username } is the Evaluated Expression. Here the final outcome is written like we can't write {if(true) username. This is not evaluated.
     <>
-      <p>Hello welcome</p>
+      <p>Hello welcome {username}</p> 
       <Coffee />
     </>
   )
 }
+//Now the question is how the variable is injected into the tree? Check the main.jsx
+
+//*  We can't write username = if(true) username because , what ever is written inside the variable that is directly injected into the tree and hence these condition may create  syntax errors
 
 export default App
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function myFunction()
+{
+  return (
+    <h1>From the custom function</h1>
+  )
+}
+
+//React compile when it gets the "App" code. Check in App.js file where we return the jsx code.
+const reactElement = {
+  type : 'a',
+  props: {
+      href : 'https://google.com',
+      target : '_blank'
+  },
+
+  children : 'Click me to visist the google'
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Click me to visit Google!</a>//+
+)
+
+const anotherUser = "BlackDevil";
+const innerObject = React.createElement(
+  //! Parameters expected by React render function.
+  'a',
+  {href : 'https://google.com', target : '_blank'},
+  'Click me to visist the google' ,
+  //At last the variable is injected
+  anotherUser // Evaluated Expression
+)
+
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
+    // myFunction() works directly
+    // anotherElement works fine
+    // reactElement can't as the react is expecting some other parameters like we did in customRender function.
+    // innerObject 
 )
