672f58bd9af9879ba30e20bbef9f124269b8ab69
Gopal <gopalmittal2003@gmail.com>
2024-10-01 19:22:52 +0530

    Components and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username : "Devil",
+    email : "devil@gmail.com",
+    description : "This is a description about the user."
+  }
 
+  let newArr = [1,2,3,4,5];
+  /* <Card username="Gopal Agarwal" btnText="click me!"/>
+  <Card username="BlackDevil" btnText="Visit Me!"/> */
   return (
     <>
         <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+        {/* <Card {myObj} /> */}
+        {/* <Card someObject = {myObj}/> */}
+        {/* <Card someObject = {newArr}/> */}
 
+        <Card username= "Gopal Agarwal" />
+        <Card username= "Black Devil" />
     </>
   )
 }
