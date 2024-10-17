5c778ef27bd3ea49f009f994e06405106559ddf2
Gopal <gopalmittal2003@gmail.com>
2024-10-17 15:38:25 +0530

    Production Grade Project Setup-Env Variables setup

---
+++ 04Projects/009megablog/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+    <>
+      <h1>A blog app in appwirte</h1>
+    </>
+  )
+}
+
+export default App
