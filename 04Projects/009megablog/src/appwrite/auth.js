9c50558691926d1b3ecc9eb3c9f6604d4ef806ec
Gopal <gopalmittal2003@gmail.com>
2024-10-18 16:05:34 +0530

    authentication Setup

---
+++ 04Projects/009megablog/src/appwrite/auth.js
@@ -0,0 +1,77 @@
+import conf from "../conf/conf";
+import {Client, Account, ID} from "appwrite";
+
+export class AuthService{
+
+    //? The Client object initializes the connection to the Appwrite backend, allowing the app to send requests (such as account creation, login, etc.) to Appwrite services.
+
+    //! In JavaScript, const and let are used to define local variables within the scope of functions, blocks, or methods. However, class properties (like client and account) are automatically attached to the instance of the class when you define them outside of a method or function, so they do not require let or const.
+
+    //! Class properties are a special feature of JavaScript classes that don't need let or const. When you define a property like client = new Client();, JavaScript automatically treats this as an instance property that will be available on every instance of the class.
+
+    client = new Client();
+    account;
+
+    //? The setEndpoint() and setProject() methods are called to define the API's base URL and the project ID associated with Appwrite.
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({email, password,name}) {
+        try{
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+
+            if(userAccount){
+                //call another method
+                return this.login({email, password});
+            }
+            else
+            {
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+        
+    }
+
+    async login({email, password})
+    {
+        try{
+            const userSession = await this.account.createEmailPasswordSession(email, password);
+            return userSession;
+        }catch(error){
+            throw error;
+        }
+    
+    }
+
+    async getCurrentUser(){
+        try{
+                return await this.account.get();
+        }catch(error) {
+            console.log("Appwrite service :: getCurrentUser " , error); 
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+        }catch(error){
+            console.log("Appwrite service :: logout :: error ", error); 
+        }
+    }
+
+    
+}
+
+const authService = new AuthService();
+
+export default authService;
