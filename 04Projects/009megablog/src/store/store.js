e71b175761923189422bc9eb0a7bba245f9f1a74
Gopal <gopalmittal2003@gmail.com>
2024-10-20 14:58:37 +0530

    components

---
+++ 04Projects/009megablog/src/store/store.js
@@ -0,0 +1,12 @@
+// import {configureStores} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit'
+
+
+const store = configureStore({
+        reducer : {
+
+        }
+
+});
+
+export default store;
